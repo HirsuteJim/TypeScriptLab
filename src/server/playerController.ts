d9a13bdebf33019982da5675628f52d7c8d6a8ff
Allison <pratt.allisons@gmail.com>
2022-02-09 19:38:05 -0500

    server boilerplate

---
+++ src/server/playerController.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import fs from 'fs';
+import path from 'path';
+import { Player, Scoreboard } from '../types';
+
+
+export const playerController = {
+
+  getScores: () => {
+
+  },
+
+  updateScores: () => {
+
+  },
+
+}
